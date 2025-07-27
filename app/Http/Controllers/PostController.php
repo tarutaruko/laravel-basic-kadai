83c12d2258904b37da16fb0ff97cf9935d3c4135
tarutaruko <tarutaruko39@gmail.com>
2025-07-27 23:17:04 +0900

    課題ORM

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class PostController extends Controller
 {
    public function index() {
       $posts = DB::table('posts')->get();
     return view('posts.index', compact('posts'));
- }
+   }
+
+   public function show($id) {
+      $posts = Post::find($id);
+
+      return view('posts.show', compact('posts'));
+   }
 }
